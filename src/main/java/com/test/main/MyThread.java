406e02e09c577b73f70e61bf1e067a735df1abb3
xiaobin.wang <1367718597@qq.com>
2018-03-26 17:31:09 +0800

    sssss

--- src/main/java/com/test/main/MyThread.java
+++ src/main/java/com/test/main/MyThread.java
@@ -1,5 +1,7 @@
 package com.test.main;
 
+import java.util.ArrayList;
+
 /**
  * @author xiaobin.wang
  * @date 2018/3/16
@@ -8,5 +10,13 @@ public class MyThread extends Thread {
     @Override
     public void run() {
         Thread.interrupted();
+    }
+
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList(10);
+        for (;arrayList.size()<10;) {
+            arrayList.add(1);
+        }
+        arrayList.add(2);
     }
 }
